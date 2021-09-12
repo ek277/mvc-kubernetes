69d1f6d69520050114e6fd7fc1ee817338593003
Ehm27 <erick@coolhumans.team>
2021-09-11 19:18:01 -0500

    Archivos de proyecto MVC-Kubernetes

---
+++ Models/ContextoCursos.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCPrototipo.Models
+{
+    public class ContextoCursos : DbContext
+    {
+        public ContextoCursos(DbContextOptions<ContextoCursos> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Instructor> Instructor{get; set;}
+    }
+}
